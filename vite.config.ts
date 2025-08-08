db4db371c6122f1d1a11f19ea8d3e80d8f5210c7
Williamarvin <Williamarvin111@gmail.com>
2025-08-08 17:50:05 +0800

    deploy github config

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: "/william_website",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
