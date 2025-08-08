42bfc91e8271b3d58c0dbf99e94f4096f506daa8
Williamarvin <Williamarvin@users.noreply.github.com>
2025-08-08 18:01:27 +0000

    deploy: 2682d098177ee40f0ea3b4925ea2466840208d1a

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var vite_config_default = defineConfig({
       )
     ] : []
   ],
-  base: "/personal_website/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
