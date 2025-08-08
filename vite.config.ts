92ab33e8c5c82c631d9bfa264bcd8dfd02805c70
Williamarvin <62838685+Williamarvin@users.noreply.github.com>
2025-08-09 02:02:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base: "/",
+  base: "/personal_website/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
